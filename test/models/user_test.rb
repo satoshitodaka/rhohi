3bc8d361cffa3f11421e7ee997bba1553b6bcbd5
satoshitodaka <satoshitodaka0705@gmail.com>
2021-04-21 07:54:29 +0900

    modified tripstatement_model_test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = users(:satoshi)
-    # @user = User.new(name: "Ai", email: "aitodaka@todaka.com")
+    # @user = User.new(name: "ai", email: "aitodaka@rhohi.com")
   end
 
   test "should be valid" do
@@ -20,11 +20,11 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  # test "associated trip_statements should be destroyed" do
-  #   @user.save
-  #   @user.trip_statements.create!(distination: "kagoshima", purpose: "recruit")
-  #   assert_difference 'Trip_Statement.count', -1 do
-  #     @user.destroy
-  #   end
-  # end
+  test "associated trip_statements should be destroyed" do
+    @user.save
+    @user.trip_statements.create!(distination: "kagoshima", purpose: "recruit", applied: false, approved: false)
+    assert_difference 'TripStatement.count', -1 do
+      @user.destroy
+    end
+  end
 end
