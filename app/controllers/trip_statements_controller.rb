3f0380c836a42df311e30c19a8a0c928111d62de
satoshitodaka <satoshitodaka0705@gmail.com>
2021-04-26 08:36:11 +0900

    add seed

--- app/controllers/trip_statements_controller.rb
+++ app/controllers/trip_statements_controller.rb
@@ -12,4 +12,5 @@ class TripStatementsController < ApplicationController
 
   def new
   end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,39 @@
 # Company
 Company.create!(name: "日立物流ファインネクスト株式会社", address: "東京都中央区京橋")
 
-# 5.times do |n|
-#   name = Faker::Company.name
-#   address = Gimei.address.kanji
-#   Company.create!(name: name, address: address)
-# end
-
 # User
 User.create!(name: "戸高 仁",
-  email: "satoshitodaka@rhohi.com",
+  email: "gkjojo0507@yahoo.co.jp",
   password: "password",
-  compnay_id: 1
+  company_id: 1
 )
 
-# users = 
-# 10.times do |n|
+5.times do |n|
+  name = Faker::Company.name
+  address = Gimei.address.kanji
+  Company.create!(name: name, address: address)
+end
+
+Company.all.each do |company|
+  n = 1
+  company.users.create!(
+    user_name = Gimei.kanji,
+    email = "example-#{n+1}@rhohi.com"
+  )
+end
+
+# companies = Company.order(:created_at).take(3)
+# 20.times do
 #   user_name = Gimei.kanji
 #   email = "example-#{n+1}@rhohi.com"
-#   User.create!(name: user_name, email: email, password: "password")
+#   companies.each { |company| company.users.create!(name: user_name, email: email, password: "password") }
+# end
 
-# TripStatement
+# # TripStatement
 
-# TripStatement.create!(distination: "tokyo", purpose: "dinner", applied: false, approved: false)
+# users = User.order(:created_at).take(15)
+# 30.times do
+#   distination = Gimei.address.kanji
+#   purpose = Faker::Lorem.sentence(word_count: 5)
+#   users.each { |user| user.trip_statements.create!(distination: distination, purpose: purpose, applied: false, approved: false) }
+# end
