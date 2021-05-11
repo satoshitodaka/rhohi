9397251e50916fa16d4ccd337e9404ce439c22fe
satoshitodaka <satoshitodaka0705@gmail.com>
2021-05-12 07:38:10 +0900

    implement show expence

--- app/controllers/expences_controller.rb
+++ app/controllers/expences_controller.rb
@@ -2,7 +2,7 @@ class ExpencesController < ApplicationController
 before_action :authenticate_user!
 
   def new
-    @trip_statement = TripStatement.find(params[:trip_statements.id])
+    @trip_statement = TripStatement.find(params[:trip_statement_id])
     @expence = @trip_statement.expences.new
   end
 
@@ -11,25 +11,29 @@ before_action :authenticate_user!
 
   def show
     @user = current_user
-    @trip_statement = TripStatement.find(params[:id])
-    @expences = @trip_statement.expences.all
+    @expence = TripStatement.find(params[:id])
+    @trip_statement = @expence.trip_statements.
+    
   end
 
   def create
+    @trip_statement = TripStatement.find(params[:trip_statement_id])
     @expence = @trip_statement.expences.build(expence_params)
     if @expence.save
-      # redirect_to 'root_url'
+      redirect_to trip_statement_path(params[:trip_statement_id])
     else
-      render 'new_trip_statement_expences_path'
+      redirect_to new_trip_statement_expence_url(@trip_statement)
     end
   end
 
   def destroy
   end
 
+
   private
     def expence_params
       params.require(:expence).permit(:date, :transportation, :bording, :get_off, :fare, :mileage, :allowance)
     end
+  end
 
 end
