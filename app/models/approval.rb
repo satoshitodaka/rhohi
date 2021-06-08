6029785b61eb07f4b460e8e806e2f0f5515ec16d
satoshitodaka <satoshitodaka0705@gmail.com>
2021-06-08 19:19:00 +0900

    minor fix

--- app/models/approval.rb
+++ app/models/approval.rb
@@ -4,4 +4,12 @@ class Approval < ApplicationRecord
   validates :trip_statement_id, presence: true
   validates :user_id, presence: true
   # validates :approval, presence: true
+
+  # after_create :add_approval_for_trip_statement
+
+  # private
+
+  # def add_approval_for_trip_statement
+  #   self.trip_statement.update(approved: true, approved_at: Time.zone.now)
+  # end
 end
