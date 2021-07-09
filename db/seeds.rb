4da76de7dfe29b9bb623db35a164746b831785bd
satoshitodaka <satoshitodaka0705@gmail.com>
2021-07-09 20:45:08 +0900

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
+# serup
+# trip_statements
+distinations = ["新宿","銀座","渋谷","品川","立川","町田","大宮","浦和","横浜","川崎"]
+purposes = ["会議","事前ミーティング","採用面接","工場見学","取引先挨拶"]
+
+# expenses
+transportations = ["JR","地下鉄","タクシー","バス"]
+bording_samples = ["新宿","有楽町","渋谷","立川","町田","大宮","浦和","横浜","川崎","八重洲北口","神田","秋葉原","飯田橋","外苑前"]
+get_off_samples = ["新宿","有楽町","渋谷","立川","町田","大宮","浦和","横浜","川崎","八重洲北口","神田","秋葉原","飯田橋","外苑前"]
+fares = [165, 170,195,200,350,500,720,810,920]
+allowances = [0,2300,3200]
+
 # Company
-Company.create!(name: "日立物流ファインネクスト株式会社", address: "東京都中央区京橋")
-# Company.create!(name: "株式会社日立物流", address: "東京都中央区京橋")
-# Company.create!(name: "株式会社日立物流東日本", address: "茨城県日立市城南町")
-# Company.create!(name: "株式会社日立物流首都圏", address: "千葉県柏市末広町")
-# Company.create!(name: "日立物流ソフトウェア株式会社", address: "東京都江東区東陽")
+Company.create!(name: "サンプル1株式会社", address: "東京都中央区京橋")
+Company.create!(name: "サンプル2株式会社", address: "東京都中央区京橋")
+Company.create!(name: "サンプル3株式会社", address: "茨城県日立市城南町")
+Company.create!(name: "サンプル4株式会社", address: "千葉県柏市末広町")
+Company.create!(name: "サンプル5株式会社", address: "東京都江東区東陽")
 
 # User
 admin = User.create!(name: "Admin_User",
@@ -21,7 +33,6 @@ User.create!(name: "Normal_User",
   password: "password",
   company_id: 1,
   birthday: "1993-07-05",
-  admin: true
 )
 
 # 5.times do
@@ -47,8 +58,8 @@ end
 # 各ユーザーに1件ずつ提出済・承認済の申請情報を作成
 User.all.each do |user|
   user.trip_statements.create!(
-    distination: Gimei.address.kanji,
-    purpose: Faker::Lorem.sentence(word_count: 3),
+    distination: distinations.sample,
+    purpose: purposes.sample,
     start_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now),
     finish_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now),
     work_done_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now - 1),
@@ -60,8 +71,8 @@ end
 # 各ユーザーに1件ずつ提出済・未承認の申請情報を作成
 User.all.each do |user|
   user.trip_statements.create!(
-    distination: Gimei.address.kanji,
-    purpose: Faker::Lorem.sentence(word_count: 3),
+    distination: distinations.sample,
+    purpose: purposes.sample,
     start_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now),
     finish_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now),
     work_done_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now - 1),
@@ -73,8 +84,8 @@ end
 # 各ユーザーに1件ずつ未提出・未承認の申請情報を作成
 User.all.each do |user|
   user.trip_statements.create!(
-    distination: Gimei.address.kanji,
-    purpose: Faker::Lorem.sentence(word_count: 3),
+    distination: distinations.sample,
+    purpose: purposes.sample,
     start_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now),
     finish_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now),
     work_done_at: Faker::Time.between(from: Time.zone.now - 30, to: Time.zone.now - 1),
@@ -87,11 +98,11 @@ end
 TripStatement.all.each do |trip_statement|
   trip_statement.expences.create!(
     date: Faker::Date.between(from: 30.days.ago, to: Date.today),
-    transportation: Faker::Lorem.sentence(word_count: 1),
-    bording: Faker::Lorem.sentence(word_count: 1),
-    get_off: Faker::Lorem.sentence(word_count: 1),
-    fare: Faker::Lorem.sentence(word_count: 3),
-    allowance: true
+    transportation: transportations.sample,
+    bording: bording_samples.sample,
+    get_off: get_off_samples.sample,
+    fare: fares.sample,
+    allowance: allowances.sample
   )
 end
 
