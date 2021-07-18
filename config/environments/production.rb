0116d66cbb50e0400ac44cceb5807d806c766876
satoshitodaka <satoshitodaka0705@gmail.com>
2021-07-18 13:29:04 +0900

    minor fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # default
+  config.action_mailer.default_url_options = { :host => 'https://rhohi.herokuapp.com' }
+
   # # SendGrid関連
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.perform_deliveries = true
