4c09ce970d97f9819cadd7ecf2b464c6105a3446
satoshitodaka <satoshitodaka0705@gmail.com>
2021-07-18 12:30:32 +0900

    add mailgun setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,17 +94,32 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # # SendGrid関連
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # host = 'rhohi.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
+  # ActionMailer::Base.smtp_settings = {
+  #    :port => 587,
+  #    :address => 'smtp.sendgrid.net',
+  #    :user_name => ENV['SENDGRID_USERNAME'],
+  #    :password => ENV['SENDGRID_PASSWORD'],
+  #    :domain => host,
+  #    :authentication => :plain,
+  #    :enable_starttls_auto => true
+  # }
+
+  # MailGun関連
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
   host = 'rhohi.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-     :port => 587,
-     :address => 'smtp.sendgrid.net',
-     :user_name => ENV['SENDGRID_USERNAME'],
-     :password => ENV['SENDGRID_PASSWORD'],
+     :port => ENV['MAILGUN_SMTP_PORT'],
+     :address => ENV['MAILGUN_SMTP_SERVER'],
+     :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+     :password => ENV['MAILGUN_SMTP_PASSWORD'],
      :domain => host,
      :authentication => :plain,
-     :enable_starttls_auto => true
   }
 end
