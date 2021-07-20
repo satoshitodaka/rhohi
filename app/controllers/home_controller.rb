c4b53e790a42421d206ee32007f477fa977e3326
satoshitodaka <satoshitodaka0705@gmail.com>
2021-07-21 07:07:48 +0900

    install rubocop

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
       # 申請中の件数を表示
       @created_statements = @user.trip_statements.left_joins(:approvals).merge(Approval.where(id: nil))
       # 管理者向け表示
-      @not_approved = TripStatement.joins(:user).where("(company_id = ?) AND (applied = ?) AND (approved = ?)", current_user.company_id, true, false)
+      @not_approved = TripStatement.joins(:user).where('(company_id = ?) AND (applied = ?) AND (approved = ?)', current_user.company_id, true, false)
     end
   end
 
